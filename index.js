39e318c13fd1275320c9d73a8cd0e62ff458a2b8
shakyanandk <44444660+shakyanandk@users.noreply.github.com>
2023-09-04 23:09:17 +0530

    cors fix

--- index.js
+++ index.js
@@ -5,7 +5,18 @@ const port = 3001;
 const db = require('./services/db');
 
 
-
+app.use((req, res, next) => {
+  // Allow requests from your React app's origin (http://localhost:3000)
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  // Allow certain HTTP methods
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  // Allow specific headers
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  // Allow cookies to be included in requests (if needed)
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  // Continue with the next middleware/route
+  next();
+});
 
 app.use(express.static('public'));
 app.use('/assets', express.static('assets'));
@@ -33,7 +44,10 @@ app.get("/api/get-products", async(req, res) => {
     const get_products_cmd = `SELECT * from products`;
 
     const result = await db.query(get_products_cmd);
-
+     
+  //   if (result[0][0].img_64 !== null) {
+  //     result[0][0].img_64 = Buffer.from(result[0][0].img_64).toString('base64');
+  //  }
     res.json({products: result[0]})
 });
 
